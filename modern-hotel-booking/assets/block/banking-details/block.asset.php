1fbbd8d81bf4bf289fa195c9be4fce9f1d14076f
leslieradue-web <leslie.radue@gmail.com>
2026-05-06 21:46:42 +0300

    Cleanup nested directory, move playground.zip to root, and update blueprint URLs

--- modern-hotel-booking/assets/block/banking-details/block.asset.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('ABSPATH')) exit;
-return array(
-    'dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components', 'wp-i18n', 'wp-server-side-render'),
-    'version' => '2.3.8',
-);
